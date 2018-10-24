bfb94608b849c1d01ff2325d0aa3a781d7febeee
wuzhiheng <335639905@qq.com>
2018-10-24 08:58:50 +0800

    问题卡和副卡管理页面添加搜索

---
+++ src/main/java/com/team/dao/TerminalSimFLowDao.java
@@ -0,0 +1,14 @@
+package com.team.dao;
+
+import java.util.Map;
+
+/**
+ * @Author : wuzhiheng
+ * @Description :
+ * @Date Created in 11:38 AM 2018/10/22
+ */
+public interface TerminalSimFLowDao {
+
+    String queryTime(Map<String,Object> map);
+
+}
